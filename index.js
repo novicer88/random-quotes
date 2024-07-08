9eaefc00727eb4135a6966597a5fd0271724bb5d
novicer88 <vitalikasmar@gmail.com>
2024-07-08 18:44:36 +0300

    generateRandomInt util function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import quotes from './src/quotes.js';
+import { generateRandomInt } from './src/utils.js';
 import {
   hideFavoriteCard,
   showFavoriteCard,
@@ -14,12 +15,12 @@ const favoritesContainer = document.getElementById('favorites-container');
 let currentQuoteIndex;
 
 function generateRandomQuote() {
-  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[currentQuoteIndex];
-  const { quote, author } = randomQuote;
+  const randomIndex = generateRandomInt(quotes.length);
+  const { quote, author, isFavorite } = quotes[randomIndex];
+  currentQuoteIndex = randomIndex;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
-  toggleFavoriteIcon(randomQuote.isFavorite, toggleFavoriteBtn);
+  toggleFavoriteIcon(isFavorite, toggleFavoriteBtn);
   toggleFavoriteBtn.style.display = 'inline-block';
 }
 
