7318558c035f42897a66d188164b302f678d65c1
novicer88 <vitalikasmar@gmail.com>
2024-07-09 20:29:10 +0300

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: "Your time is limited, so don't waste it living someone else's life.",
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    author: 'John Lennon',
+    isFavorite: false,
+  },
+  {
+    text: 'Get busy living or get busy dying.',
+    author: 'Stephen King',
+    isFavorite: false,
+  },
+  {
+    text: 'You only live once, but if you do it right, once is enough.',
+    author: 'Mae West',
+    isFavorite: false,
+  },
+  {
+    text: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+    author: 'Thomas Edison',
+    isFavorite: false,
+  },
+  {
+    text: 'If you want to live a happy life, tie it to a goal, not to people or things.',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: 'Never let the fear of striking out keep you from playing the game.',
+    author: 'Babe Ruth',
+    isFavorite: false,
+  },
+  {
+    text: "Money and success don't change people; they merely amplify what is already there.",
+    author: 'Will Smith',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
