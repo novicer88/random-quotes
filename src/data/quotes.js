7cefe066798e243f0c513ea44bda3a3892b798f4
novicer88 <vitalikasmar@gmail.com>
2024-07-10 20:55:52 +0300

    Added unique ids to the array of quotes

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,50 +1,60 @@
 const quotes = [
   {
+    id: 'bf96f091',
     text: 'The only way to do great work is to love what you do',
     author: 'Steve Jobs',
     isFavorite: false,
   },
   {
+    id: 'e9c1bba9',
     text: 'Innovation distinguishes between a leader and a follower',
     author: 'Steve Jobs',
     isFavorite: false,
   },
   {
+    id: '4217dc28',
     text: "Your time is limited, so don't waste it living someone else's life.",
     author: 'Steve Jobs',
     isFavorite: false,
   },
   {
+    id: '88d9ca45',
     text: "Life is what happens when you're busy making other plans.",
     author: 'John Lennon',
     isFavorite: false,
   },
   {
+    id: '964084b8',
     text: 'Get busy living or get busy dying.',
     author: 'Stephen King',
     isFavorite: false,
   },
   {
+    id: '8eb3fb1f',
     text: 'You only live once, but if you do it right, once is enough.',
     author: 'Mae West',
     isFavorite: false,
   },
   {
+    id: 'd9dj3kd3',
     text: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
     author: 'Thomas Edison',
     isFavorite: false,
   },
   {
+    id: '3948dn3n',
     text: 'If you want to live a happy life, tie it to a goal, not to people or things.',
     author: 'Albert Einstein',
     isFavorite: false,
   },
   {
+    id: '12jd4jd9',
     text: 'Never let the fear of striking out keep you from playing the game.',
     author: 'Babe Ruth',
     isFavorite: false,
   },
   {
+    id: 'dj30d9sm',
     text: "Money and success don't change people; they merely amplify what is already there.",
     author: 'Will Smith',
     isFavorite: false,
